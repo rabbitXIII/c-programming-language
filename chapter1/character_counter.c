0de37d9dd7b51335aab4583cd3d8c9febd40ef0f
Rohit <rohitgopal@gmail.com>
2013-04-02 17:51:22 +0000

    added vim swap to gitignore, updated character counter to version 2

--- chapter1/character_counter.c
+++ chapter1/character_counter.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/* count characters in input */
+/* count characters in input version 2 */
 
 main() {
-	long character_count;
-	character_count = 0;
-	while( getchar() != EOF ) 
-		++character_count;
-	printf("%ld\n", character_count);
+	double character_count;
+	for(character_count = 0; getchar() != EOF; ++character_count) 
+		; // null statement is valid in c. for loop needs to have a body
+	printf("%.0f\n", character_count); //%f is for both float and double
 }
