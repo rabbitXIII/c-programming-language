bfeedb8a801b4d2abfd5edfef04fa1c112b0724f
Rohit <rohitgopal@gmail.com>
2013-04-02 19:09:51 +0000

    added function from Exercise 1-15 to temp_conversion_table.c

--- chapter1/temp_conversion_table.c
+++ chapter1/temp_conversion_table.c
@@ -5,9 +5,13 @@
 #define LOWER_LIMIT 	0
 #define UPPER_LIMIT 	300
 #define STEP_SIZE 	20
-
+double conversion( int fahr );
 main() {
 	int fahr;
 	for (fahr = LOWER_LIMIT; fahr <= UPPER_LIMIT; fahr = fahr + STEP_SIZE)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+		printf("%3d %6.1f\n", fahr, conversion(fahr));
+}
+
+double conversion( int fahr ) {
+	return (5.0/9.0)*(fahr-32);
 }
