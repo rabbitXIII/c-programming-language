93e5bd2d24ac4df6dc6ebba1d82200ee629701cb
Rohit <rohitgopal@gmail.com>
2013-04-02 13:45:01 +0000

    changed to using defined constants and for loop

--- chapter1/temp_conversion_table.c
+++ chapter1/temp_conversion_table.c
@@ -2,19 +2,12 @@
 
 /* print the Fahrenheit-Celsius table for fahr=0, 20, ..., 300 */
 
+#define LOWER_LIMIT 	0
+#define UPPER_LIMIT 	300
+#define STEP_SIZE 	20
+
 main() {
-	float fahr, celsius;
-	float lower_limit, upper_limit, step_size;
-
-	lower_limit = 0;
-	upper_limit = 300;
-	step_size = 20;
-
-	fahr = lower_limit;
-
-	while (fahr <= upper_limit) {
-		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f%6.1f\n", fahr, celsius);
-		fahr = fahr + step_size;
-	}
+	int fahr;
+	for (fahr = LOWER_LIMIT; fahr <= UPPER_LIMIT; fahr = fahr + STEP_SIZE)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
