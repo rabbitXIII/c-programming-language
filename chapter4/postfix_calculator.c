68cff616acaa4e27af8836346ec0133520d2c226
Rohit <rohitgopal@gmail.com>
2013-04-05 00:41:49 +0000

    started 4.3 - postfix calculator

---
+++ chapter4/postfix_calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_OPERAND_SIZE 100
+#define NUMBER_FOUND '0'
+
+int get_op_character( char [] );
+void push( double );
+double pop( void );
+
+/* post-fix calculator */
